286042ba6b49b693d543128c22b9868766f7fad3
Joaquin144 <vvpp898.comco@gmail.com>
2023-07-09 14:27:54 +0530

    Navbar, footer, grafbase schema

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+import {NavLinks} from "@/constants";
+import AuthProviders from "@/components/AuthProviders";
+
+const Navbar = () => {
+    const session = {};
+    return (
+        <nav className="flexBetween navbar">
+            <div className="flex-1 flexStart gap-10">
+                {/*Home Page will have '/' route*/}
+                <Link href="/">
+                    <Image src="/logo_three.png" alt="Inspire Shot logo" width={226} height={86}/>
+                </Link>
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="flexCenter gap-4">
+                {session ? (
+                    <>
+                        UserPhoto
+                        <Link href="/create-project">Share Work</Link>
+                    </>
+                ): (
+                    <AuthProviders/>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
